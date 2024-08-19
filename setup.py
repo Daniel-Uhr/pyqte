8cf8bcc590f2d377ac310503db19b0e2c976fffd
Daniel de Abreu Pereira Uhr <61239189+Daniel-Uhr@users.noreply.github.com>
2024-08-19 12:58:46 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyqte',
-    version='0.1.0',
-    author='Daniel de Abreu Pereira Uhr',
-    author_email='daniel.uhr@gmail.com',
-    description='A Python package for estimating Quantile Treatment Effects (QTE) and related estimators for causal inference.',
+    name="pyqte",
+    version="1.0.0",
+    author="Daniel de Abreu Pereira Uhr",
+    author_email="daniel.uhr@gmail.com",
+    description="A Python package for estimating Quantile Treatment Effects (QTE) using R's qte package via rpy2.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/pyqte',
+    url="https://github.com/Daniel-Uhr/pyqte",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
-        'numpy',
-        'pandas',
-        'statsmodels',
-        'matplotlib',
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "scipy",
+        "statsmodels",
+        "rpy2"
     ],
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    include_package_data=True,
 )
