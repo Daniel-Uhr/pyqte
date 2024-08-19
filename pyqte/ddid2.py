840f345593a35e4e995e9aa4b5d48d4d374e396b
Daniel de Abreu Pereira Uhr <61239189+Daniel-Uhr@users.noreply.github.com>
2024-08-19 14:58:44 -0300

    Update ddid2.py

--- pyqte/ddid2.py
+++ pyqte/ddid2.py
@@ -1,95 +1,137 @@
-import numpy as np
 import pandas as pd
-from scipy.stats import norm
-from statsmodels.regression.quantile_regression import QuantReg
+from rpy2.robjects import r, Formula
+from rpy2.robjects.packages import importr
+from rpy2.robjects import pandas2ri
+from .data_loader import prepare_r_data, create_formula
 
-class DDiD2Estimator:
+# Activate the pandas conversion to R dataframes
+pandas2ri.activate()
+
+# Import the necessary R packages
+qte = importr('qte')
+
+class DDID2Estimator:
     """
-    DDiD2Estimator is used to estimate Quantile Treatment Effects using a Difference-in-Differences approach.
+    DDID2Estimator is used to estimate the Difference-in-Differences with two differences (DDID2) effects.
     
     Attributes:
     -----------
     data : pandas.DataFrame
-        The dataset containing the outcome variable, treatment indicator, time indicator, and covariates.
+        The dataset containing the outcome variable, treatment indicator, and covariates.
     outcome : str
         The name of the outcome variable.
     treatment : str
         The name of the treatment indicator variable.
-    time : str
-        The name of the time indicator variable.
-    covariates : list of str
-        The list of covariates to control for in the estimation.
-    quantiles : list of float
-        The list of quantiles at which to estimate the treatment effect.
+    time_var : str
+        The name of the time variable.
+    id_var : str
+        The name of the individual identifier variable.
     """
     
-    def __init__(self, data, outcome, treatment, time, covariates, quantiles):
-        self.data = data
+    def __init__(self, data, outcome, treatment, time_var, id_var, covariates=None):
+        self.data = prepare_r_data(data)
         self.outcome = outcome
         self.treatment = treatment
-        self.time = time
-        self.covariates = covariates
-        self.quantiles = quantiles
-        
-    def estimate(self):
-        """
-        Estimate the Quantile Treatment Effects using DDiD2 for the specified quantiles.
-        
-        Returns:
-        --------
-        ddid2_results : pandas.DataFrame
-            A DataFrame containing the estimated Quantile Treatment Effects and their standard errors.
-        """
-        ddid2_results = []
-        
-        for quantile in self.quantiles:
-            pre_treated_data = self.data[(self.data[self.treatment] == 1) & (self.data[self.time] == 0)]
-            post_treated_data = self.data[(self.data[self.treatment] == 1) & (self.data[self.time] == 1)]
-            pre_control_data = self.data[(self.data[self.treatment] == 0) & (self.data[self.time] == 0)]
-            post_control_data = self.data[(self.data[self.treatment] == 0) & (self.data[self.time] == 1)]
-            
-            model_pre_treated = QuantReg(pre_treated_data[self.outcome], pre_treated_data[self.covariates])
-            result_pre_treated = model_pre_treated.fit(q=quantile)
-            
-            model_post_treated = QuantReg(post_treated_data[self.outcome], post_treated_data[self.covariates])
-            result_post_treated = model_post_treated.fit(q=quantile)
-            
-            model_pre_control = QuantReg(pre_control_data[self.outcome], pre_control_data[self.covariates])
-            result_pre_control = model_pre_control.fit(q=quantile)
-            
-            model_post_control = QuantReg(post_control_data[self.outcome], post_control_data[self.covariates])
-            result_post_control = model_post_control.fit(q=quantile)
-            
-            qtete_pre = result_pre_treated.params - result_pre_control.params
-            qtete_post = result_post_treated.params - result_post_control.params
-            
-            qtete_diff = qtete_post - qtete_pre
-            se_pre = np.sqrt(result_pre_treated.bse**2 + result_pre_control.bse**2)
-            se_post = np.sqrt(result_post_treated.bse**2 + result_post_control.bse**2)
-            se_diff = np.sqrt(se_pre**2 + se_post**2)
-            
-            ddid2_results.append({
-                "Quantile": quantile,
-                "DDiD2 Estimate": qtete_diff[self.covariates[0]],  # Focusing on the first covariate for simplicity
-                "Std. Error": se_diff[self.covariates[0]]
-            })
-        
-        return pd.DataFrame(ddid2_results)
+        self.time_var = time_var
+        self.id_var = id_var
+        self.covariates = covariates if covariates else []
 
-    def plot_ddid2(self, ddid2_results):
+    def estimate(self, t, tmin1, tmin2, probs=None, se=True, iters=100, alp=0.05):
         """
-        Plot the estimated DDiD2 Quantile Treatment Effects.
+        Estimate the DDID2 effect.
         
         Parameters:
         -----------
-        ddid2_results : pandas.DataFrame
-            The DataFrame containing the estimated DDiD2 QTETs to be plotted.
+        t : int
+            The third time period (post-treatment).
+        tmin1 : int
+            The second time period (pre-treatment).
+        tmin2 : int
+            The first time period (pre-treatment).
+        probs : list of float, optional
+            The list of quantiles at which to estimate the treatment effect.
+        se : bool, optional
+            Whether to compute standard errors (default is True).
+        iters : int, optional
+            The number of bootstrap iterations for standard errors (default is 100).
+        alp : float, optional
+            The significance level for confidence intervals (default is 0.05).
+        
+        Returns:
+        --------
+        results : dict
+            A dictionary containing the estimated DDID2 effects and standard errors.
+        """
+        formula_str = f"{self.outcome} ~ {self.treatment}"
+        formula = create_formula(self.outcome, [self.treatment] + self.covariates)
+        
+        ddid2_result = qte.ddid2(
+            formla=Formula(formula_str),
+            t=t,
+            tmin1=tmin1,
+            tmin2=tmin2,
+            tname=self.time_var,
+            idname=self.id_var,
+            data=self.data,
+            probs=r.probs if probs else r.seq(0.05, 0.95, 0.05),
+            se=se,
+            iters=iters,
+            alp=alp
+        )
+        
+        # Extract results from the R object
+        qte_estimates = dict(zip(ddid2_result.rx2('probs'), ddid2_result.rx2('qte')))
+        se_estimates = dict(zip(ddid2_result.rx2('probs'), ddid2_result.rx2('qte.se')))
+        ate_estimate = ddid2_result.rx2('ate')[0]
+        ate_se_estimate = ddid2_result.rx2('ate.se')[0]
+        
+        results = {
+            'qte': qte_estimates,
+            'qte.se': se_estimates,
+            'ate': ate_estimate,
+            'ate.se': ate_se_estimate
+        }
+        
+        return results
+
+    def summary(self, results):
+        """
+        Generate a summary of the DDID2 estimation results.
+        
+        Parameters:
+        -----------
+        results : dict
+            The results from the DDID2 estimation.
+        
+        Returns:
+        --------
+        summary : str
+            A summary of the DDID2 estimation results.
+        """
+        summary_str = "DDID2 Estimation Results:\n"
+        summary_str += "Quantile Treatment Effects (QTE):\n"
+        for prob, qte in results['qte'].items():
+            summary_str += f"Quantile {prob:.2f}: QTE = {qte:.4f}, SE = {results['qte.se'][prob]:.4f}\n"
+        summary_str += f"\nAverage Treatment Effect (ATE): {results['ate']:.4f}, SE: {results['ate.se']:.4f}\n"
+        return summary_str
+
+    def plot(self, results):
+        """
+        Plot the DDID2 estimation results.
+        
+        Parameters:
+        -----------
+        results : dict
+            The results from the DDID2 estimation.
         """
         import matplotlib.pyplot as plt
         
-        plt.errorbar(ddid2_results["Quantile"], ddid2_results["DDiD2 Estimate"], yerr=ddid2_results["Std. Error"], fmt='o')
-        plt.xlabel("Quantile")
-        plt.ylabel("DDiD2 Estimate")
-        plt.title("Quantile Treatment Effects using DDiD2")
+        quantiles = list(results['qte'].keys())
+        qte_values = list(results['qte'].values())
+        qte_se_values = list(results['qte.se'].values())
+        
+        plt.errorbar(quantiles, qte_values, yerr=qte_se_values, fmt='o')
+        plt.xlabel('Quantiles')
+        plt.ylabel('QTE')
+        plt.title('DDID2: Quantile Treatment Effects')
         plt.show()
-
