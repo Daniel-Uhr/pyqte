631d3a71ced1997b96f5d11a10d2a0b823910d3c
Daniel de Abreu Pereira Uhr <61239189+Daniel-Uhr@users.noreply.github.com>
2024-08-19 23:56:01 -0300

    Update panel_qtet.py

--- pyqte/panel_qtet.py
+++ pyqte/panel_qtet.py
@@ -4,6 +4,7 @@ import rpy2.robjects as ro
 from rpy2.robjects import pandas2ri
 from rpy2.robjects.packages import importr
 from rpy2.robjects.conversion import localconverter
+from rpy2.robjects import Formula
 import matplotlib.pyplot as plt
 
 # Ativando a conversão automática de pandas DataFrames para R data.frames
