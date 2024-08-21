a7cab4553f3a1121fcb6891b5e2184c4693152b3
Daniel de Abreu Pereira Uhr <61239189+Daniel-Uhr@users.noreply.github.com>
2024-08-21 15:21:52 -0300

    Update spatt.py

--- pyqte/spatt.py
+++ pyqte/spatt.py
@@ -12,11 +12,6 @@ pandas2ri.activate()
 qte = importr('qte')
 
 class SpATT_Estimator:
-    """
-    SpATT_Estimator is used to estimate Spatial Average Treatment on the Treated (SpATT)
-    effects using the R 'qte' package via rpy2.
-    """
-    
     def __init__(self, formula, data, t, tmin1, tname, xformla=None, w=None, panel=False, idname=None, 
                  iters=100, alp=0.05, method="logit", se=True, 
                  retEachIter=False, seedvec=None, pl=False, cores=2):
@@ -72,20 +67,25 @@ class SpATT_Estimator:
         if self.panel:
             additional_args['panel'] = self.panel
 
-        self.result = qte.spatt(
-            formla=r_formula,
-            data=r_data,
-            **additional_args
-        )
-        self._extract_info()
+        try:
+            self.result = qte.spatt(
+                formla=r_formula,
+                data=r_data,
+                **additional_args
+            )
+        except Exception as e:
+            print(f"Error fitting the model: {e}")
+        
+        if self.result:
+            self._extract_info()
 
     def _extract_info(self):
-        self.info['qte'] = np.array(self.result.rx2('qte'))
-        self.info['probs'] = np.array(self.result.rx2('probs'))
+        self.info['qte'] = np.array(self.result.rx2('qte')) if 'qte' in self.result.names else None
+        self.info['probs'] = np.array(self.result.rx2('probs')) if 'probs' in self.result.names else None
 
         if self.se:
-            self.info['qte.lower'] = np.array(self.result.rx2('qte.lower'))
-            self.info['qte.upper'] = np.array(self.result.rx2('qte.upper'))
+            self.info['qte.lower'] = np.array(self.result.rx2('qte.lower')) if 'qte.lower' in self.result.names else None
+            self.info['qte.upper'] = np.array(self.result.rx2('qte.upper')) if 'qte.upper' in self.result.names else None
         else:
             self.info['qte.lower'] = None
             self.info['qte.upper'] = None
@@ -93,9 +93,9 @@ class SpATT_Estimator:
     def summary(self):
         if self.result is None:
             raise ValueError("Model has not been fitted yet. Call `fit()` before calling `summary()`.")
-        summary = ro.r.summary(self.result)
-        print(summary)
-        return summary
-
-
-
+        try:
+            summary = ro.r.summary(self.result)
+            print(summary)
+            return summary
+        except Exception as e:
+            print(f"Error summarizing the model: {e}")
